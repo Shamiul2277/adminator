cd69963544be486d4c439a2ac02731099924ab91
Shamiul2277 <skabir2277@gmail.com>
2022-12-08 09:37:14 +0600

    Add some component for shop management

---
+++ app/Http/Livewire/Admin/OrderManageComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class OrderManageComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.order-manage-component');
+    }
+}
---
+++ app/Http/Livewire/OrderCancelComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class OrderCancelComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.order-cancel-component');
+    }
+}
