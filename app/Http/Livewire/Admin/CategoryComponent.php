dfb247c0e1db1fe1d95d2f0d84008bc1b5d6630c
Shamiul2277 <skabir2277@gmail.com>
2022-12-09 17:16:30 +0600

    Add admin category page

--- app/Http/Livewire/Admin/CategoryComponent.php
+++ app/Http/Livewire/Admin/CategoryComponent.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Category;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 class CategoryComponent extends Component
@@ -20,6 +21,7 @@ class CategoryComponent extends Component
     }
     public function render()
     {
-        return view('livewire.admin.category-component')->layout( 'layouts/admin' );
+        $categories= Category::all();
+        return view('livewire.admin.category-component',['categories'=>$categories])->layout( 'layouts/admin' );
     }
 }
--- resources/views/livewire/admin/category-component.blade.php
+++ resources/views/livewire/admin/category-component.blade.php
@@ -28,4 +28,46 @@
     @enderror
         <button type="submit" class="btn btn-primary">Save</button>
     </form>
+
+
+
+    <div class="card">
+        <div class="card-header">
+            <h3>All Categories</h3>
+            <div class="">
+                <a href="" class="btn btn-primary  flot-end"><i class="fa fa-plus"></i> Add New Category</a>
+            </div>
+        </div>
+        <div class="card-body pt-2">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Sl No</th>
+                        <th>Banner</th>
+                        <th>Title</th>
+                        <th>Slug</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php
+                        $i=1;
+                    @endphp
+                    @foreach ($categories as $cat )
+                    <tr>
+                        <td>{{ $i }}</td>
+                        <td>{{ $cat->banner }}</td>
+                        <td>{{ $cat->category }}</td>
+                        <td>{{ $cat->slug }}</td>
+                        <td>{{ $cat->banner }}</td>
+                    </tr>
+                        @php
+                            $i++;
+                        @endphp
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
